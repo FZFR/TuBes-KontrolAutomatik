042bc8b8dbf9432453f6cbc59ee67bc71ee1488a
Fazzafr <35617149+Fazzafr@users.noreply.github.com>
2018-03-20 11:39:13 +0700

    Update PID_servo-ultra.ino

--- src/arduino/PID_servo-ultra.ino
+++ src/arduino/PID_servo-ultra.ino
@@ -33,8 +33,8 @@ void setup() {
   }
 
 void loop() {
-
-  error = sp - jarak;
+ int cm = sonar.ping_cm();
+  error = sp - cm;
   p = error * kp;
   jumlahe = error + errorx;
   i = ki * jumlahe;
@@ -62,7 +62,82 @@ void loop() {
   digitalWrite(TriggerPin, LOW);
   durasi = pulseIn(EchoPin, HIGH);
   // jarak = (durasi/2) / 29.1;
-  int cm = sonar.ping_cm();
+  // int cm = sonar.ping_cm();
+  Serial.print(cm);
+  
+  Serial.println(" cm");
+  Serial.print("PID= ");
+  Serial.println(pidx);
+  delay(60);
+
+ errorx = error;
+
+}
+
+// Servo akibat sensor ultrasonic 
+
+#include <Servo.h>
+#include <NewPing.h> // library included
+
+
+const int ServoPin = 10; // input pin servo(signal/control)
+const int TriggerPin = 3; // Trigger pin HC-SR04
+const int EchoPin = 2; // Echo  HC-SR04
+
+// 40 = jarak maks terbaca
+NewPing sonar(TriggerPin, EchoPin, 40);
+
+Servo servo;
+
+
+int sp = null; // setpoint
+
+float kp = null;
+float ki = null;
+float kd = null;
+
+float p,i,d,pid,pidx;
+float error,errorx,jumlahe;
+long durasi, jarak;
+
+void setup() {
+  Serial.begin (9600);
+  servo.attach(ServoPin);  
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+  }
+
+void loop() {
+ int cm = sonar.ping_cm();
+  error = sp - cm;
+  p = error * kp;
+  jumlahe = error + errorx;
+  i = ki * jumlahe;
+  d = error - errorx;
+  pid = p + i + d;
+  pid = 250.0 - pid;
+ 
+  pidx = sp - pid;
+ 
+  servo.write(pidx);
+
+  if(pidx < 1){
+  pidx = 0;
+  }
+ 
+  if(pidx > 250){
+  pidx = 250;
+  }
+
+
+  digitalWrite(TriggerPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(TriggerPin, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(TriggerPin, LOW);
+  durasi = pulseIn(EchoPin, HIGH);
+  // jarak = (durasi/2) / 29.1;
+  // int cm = sonar.ping_cm();
   Serial.print(cm);
   
   Serial.println(" cm");
